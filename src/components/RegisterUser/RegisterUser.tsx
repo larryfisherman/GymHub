8473ec5fe22189b6baab18ef92ec6dacf06db815
larryfisherman <szczepannowak87@gmail.com>
2023-03-29 15:44:15 +0200

    register possibility

--- src/components/RegisterUser/RegisterUser.tsx
+++ src/components/RegisterUser/RegisterUser.tsx
@@ -1,17 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
+import axios from "axios";
 
 export const RegisterUser = () => {
+  const [emailValue, setEmailValue] = useState("");
+  const [passwordValue, setPasswordValue] = useState("");
+
   return (
     <Container>
-      <Content></Content>
+      <Content>
+        Email:
+        <EmailInput
+          value={emailValue}
+          onChange={(e) => setEmailValue(e.target.value)}
+        />
+        Password:
+        <PasswordInput
+          value={passwordValue}
+          onChange={(e) => setPasswordValue(e.target.value)}
+        />
+        Confirm:
+        <ConfirmButton
+          onClick={() =>
+            axios.post("https://localhost:44390/api/user/register", {
+              email: emailValue,
+              password: passwordValue,
+            })
+          }
+        >
+          Log in!
+        </ConfirmButton>
+      </Content>
     </Container>
   );
 };
 
 const Container = styled.div`
   width: 100%;
-  height: 100%;
-  background-color: rgba(0, 0, 0, 0.5);
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
-const Content = styled.div``;
+const Content = styled.div`
+  background-color: rgba(0, 0, 0, 0.5);
+  width: 50%;
+  height: 80%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  padding: 50px;
+  & > input {
+    width: 50%;
+  }
+`;
+
+const EmailInput = styled.input``;
+const PasswordInput = styled.input``;
+const ConfirmButton = styled.button`
+  width: 10%;
+  heig
+`;
